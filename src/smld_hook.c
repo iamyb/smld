b345a06559a1da7a184c7c7b06bbe92b43a02873
iamyb <b20yang@outlook.com>
2017-01-04 00:01:50 +0800

    Update smld_hook.c

--- src/smld_hook.c
+++ src/smld_hook.c
@@ -34,49 +34,49 @@ static void  (*freep)(void*)              = NULL;
 
 static void __attribute__((constructor))smldhook_create_shm(void)
 {
-	int fd = shm_open(SMLDHOOK_SHM_NAME, O_CREAT|O_RDWR|O_EXCL,0666);
-	if(fd == -1) 
-	{
-		if((fd = shm_open(SMLDHOOK_SHM_NAME, O_RDWR, 0666))==-1)
-			handle_error("shm_open");
-	}
+    int fd = shm_open(SMLDHOOK_SHM_NAME, O_CREAT|O_RDWR|O_EXCL,0666);
+    if(fd == -1) 
+    {
+        if((fd = shm_open(SMLDHOOK_SHM_NAME, O_RDWR, 0666))==-1)
+            handle_error("shm_open");
+    }
 
-	if(ftruncate(fd, SMLDHOOK_SHM_SIZE) == -1)
-		handle_error("ftruncate");
+    if(ftruncate(fd, SMLDHOOK_SHM_SIZE) == -1)
+        handle_error("ftruncate");
 
-	smld_enabled=mmap(NULL, SMLDHOOK_SHM_SIZE, PROT_READ, MAP_SHARED, fd, 0);
-	if(smld_enabled == MAP_FAILED)
-		handle_error("mmap");
+    smld_enabled=mmap(NULL, SMLDHOOK_SHM_SIZE, PROT_READ, MAP_SHARED, fd, 0);
+    if(smld_enabled == MAP_FAILED)
+        handle_error("mmap");
 }
 
 static void __attribute__((destructor))smldhook_destroy_shm(void)
 {
-	if(munmap((void*)smld_enabled, SMLDHOOK_SHM_SIZE) == -1)
-		handle_error("munmap");
+    if(munmap((void*)smld_enabled, SMLDHOOK_SHM_SIZE) == -1)
+        handle_error("munmap");
 
-	smld_enabled = NULL;
-	
-	if(shm_unlink(SMLDHOOK_SHM_NAME) == -1);
-	//	handle_error("shm_unlink");
+    smld_enabled = NULL;
+    
+    if(shm_unlink(SMLDHOOK_SHM_NAME) == -1);
+    //    handle_error("shm_unlink");
 }
 
 static void __attribute__((constructor))smldhook_init_hook(void)
 {
-	callocp = (void*(*)(size_t, size_t))dlsym(RTLD_NEXT,"calloc");
-	mallocp = (void*(*)(size_t))dlsym(RTLD_NEXT, "malloc");
-	reallocp = (void*(*)(void*, size_t))dlsym(RTLD_NEXT,"realloc");
-	memalignp=(void*(*)(size_t,size_t))dlsym(RTLD_NEXT,"memalign");
-	freep = (void(*)(void*))dlsym(RTLD_NEXT,"free");
+    callocp = (void*(*)(size_t, size_t))dlsym(RTLD_NEXT,"calloc");
+    mallocp = (void*(*)(size_t))dlsym(RTLD_NEXT, "malloc");
+    reallocp = (void*(*)(void*, size_t))dlsym(RTLD_NEXT,"realloc");
+    memalignp=(void*(*)(size_t,size_t))dlsym(RTLD_NEXT,"memalign");
+    freep = (void(*)(void*))dlsym(RTLD_NEXT,"free");
 }
 
 /*---------------------------------------------------------------------------*/
 void* malloc(size_t len)
 {
-	void* ptr = (*mallocp)(len);
-	if(smld_enabled!=NULL&&*smld_enabled && ptr!=NULL){
-		smldhash_put64((uint64_t)ptr, (uint64_t)ptr);
-	}
-	return ptr;
+    void* ptr = (*mallocp)(len);
+    if(smld_enabled!=NULL&&*smld_enabled && ptr!=NULL){
+        smldhash_put64((uint64_t)ptr, (uint64_t)ptr);
+    }
+    return ptr;
 }
 
 /*---------------------------------------------------------------------------*/
@@ -84,51 +84,50 @@ void* malloc(size_t len)
 static int calloc_buf[1000];
 static void* calloc_wa(size_t nmemb, size_t size)
 {
-	return (void*)calloc_buf;
+    return (void*)calloc_buf;
 }
 
 /*---------------------------------------------------------------------------*/
 void* calloc(size_t nmemb, size_t size)
 {
-	if(callocp == NULL)
-		return calloc_wa(nmemb, size);
+    if(callocp == NULL)
+        return calloc_wa(nmemb, size);
 
-	void *ptr = (*callocp)(nmemb, size);
+    void *ptr = (*callocp)(nmemb, size);
 #if 0
-	// calloc just call malloc to allocate memory
-	if(smld_enabled!=NULL&&*smld_enabled && ptr!=NULL){
-		smldhash_put64((uint64_t)ptr, (uint64_t)ptr);
-	}
+    // calloc just call malloc to allocate memory
+    if(smld_enabled!=NULL&&*smld_enabled && ptr!=NULL){
+        smldhash_put64((uint64_t)ptr, (uint64_t)ptr);
+    }
 #endif
-	return ptr;
+    return ptr;
 }
 
 /*---------------------------------------------------------------------------*/
 void* memalign(size_t alignment, size_t size)
 {
-	void *ptr = (*memalignp)(alignment, size);
-	if(smld_enabled!=NULL&&*smld_enabled && ptr!=NULL){
-		smldhash_put64((uint64_t)ptr, (uint64_t)ptr);
-	}
-	return ptr;
+    void *ptr = (*memalignp)(alignment, size);
+    if(smld_enabled!=NULL&&*smld_enabled && ptr!=NULL){
+        smldhash_put64((uint64_t)ptr, (uint64_t)ptr);
+    }
+    return ptr;
 }
 
 /*---------------------------------------------------------------------------*/
 void* realloc(void*ptr, size_t size)
 {
-	void *tptr = (*reallocp)(ptr, size);
-	if(smld_enabled!=NULL&&*smld_enabled && tptr!=NULL){
-		smldhash_put64((uint64_t)tptr, (uint64_t)tptr);
-	}
-	return tptr;
+    void *tptr = (*reallocp)(ptr, size);
+    if(smld_enabled!=NULL&&*smld_enabled && tptr!=NULL){
+        smldhash_put64((uint64_t)tptr, (uint64_t)tptr);
+    }
+    return tptr;
 }
 
 /*---------------------------------------------------------------------------*/
 void free(void* ptr)
 {
-	if(smld_enabled!=NULL&&*smld_enabled && ptr!=NULL){
-		smldhash_remove64((uint64_t)ptr);
-	}
-	return (*freep)(ptr);
+    if(smld_enabled!=NULL&&*smld_enabled && ptr!=NULL){
+        smldhash_remove64((uint64_t)ptr);
+    }
+    return (*freep)(ptr);
 }
-
